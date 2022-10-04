f8d697bc234dd68f2c9153f3a84363856d91245b
zackmolt <fkrihkl@gmail.com>
2022-10-04 20:16:24 +0700

    Add RepositoryModule to AppComponent

--- app/src/main/java/com/example/tipicobundesligaapp/common/di/component/AppComponent.kt
+++ app/src/main/java/com/example/tipicobundesligaapp/common/di/component/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.example.tipicobundesligaapp.common.di.component
 
 import com.example.tipicobundesligaapp.common.data.remote.ApiService
+import com.example.tipicobundesligaapp.common.di.module.RepositoryModule
 import com.example.tipicobundesligaapp.common.di.module.RetrofitModule
 import com.example.tipicobundesligaapp.common.di.module.ViewModelFactoryModule
 import com.example.tipicobundesligaapp.common.di.module.ViewModelModule
@@ -11,7 +12,7 @@ import javax.inject.Singleton
 
 @DataScope
 @Singleton
-@Component(modules = [ViewModelModule::class, ViewModelFactoryModule::class, RetrofitModule::class])
+@Component(modules = [ViewModelModule::class, ViewModelFactoryModule::class, RetrofitModule::class, RepositoryModule::class])
 interface AppComponent {
     fun inject(leagueInfoActivity: LeagueInfoActivity)
 }
