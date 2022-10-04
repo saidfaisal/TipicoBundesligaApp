f85a8bf06715892aaa7f587d29781756f5ea013d
zackmolt <fkrihkl@gmail.com>
2022-10-04 20:16:02 +0700

    Refactor league UseCase

--- app/src/main/java/com/example/tipicobundesligaapp/usecase/GetLeagueUseCase.kt
+++ app/src/main/java/com/example/tipicobundesligaapp/usecase/GetLeagueUseCase.kt
@@ -1,9 +1,14 @@
 package com.example.tipicobundesligaapp.usecase
 
-import com.example.tipicobundesligaapp.common.data.remote.ApiService
+import com.example.tipicobundesligaapp.repository.LeagueRepository
 import javax.inject.Inject
 
 
-class GetLeagueUseCase @Inject constructor(private var apiService: ApiService) {
-    suspend fun fetchingData(t: String, id: String) = apiService.getLeague(t = t, id = id)
+class GetLeagueUseCase @Inject constructor(private var leagueRepository: LeagueRepository) {
+    suspend fun fetchingData(params: Params) = leagueRepository.getLeague(type = params.type, id = params.id)
+
+    data class Params (
+        val type: String,
+        val id: String
+    )
 }
